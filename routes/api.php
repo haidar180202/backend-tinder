eaff1065c9a453e503dd02d5382435ff1a0bbbbd
haidar180202 <gshaidar6@gmail.com>
2025-10-18 00:07:49 +0700

    feat: Implement user registration and login

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
     Route::get('/users/recommended', [UserController::class, 'getRecommendedUsers']);
     Route::post('/users/{id}/like', [UserController::class, 'likeUser']);
     Route::post('/users/{id}/dislike', [UserController::class, 'dislikeUser']);
